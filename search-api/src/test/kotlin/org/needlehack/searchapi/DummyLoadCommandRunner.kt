33e884005f166f9cd9fa9963b932b5e5fdda82ae
David Romero <davromalc@gmail.com>
2019-07-25 18:43:00 +0200

    [NH-010] - closes #10 - End to End test passing.
    Why: We need an end to end test in charge of verify the behavior of the component

--- search-api/src/test/kotlin/org/needlehack/searchapi/DummyLoadCommandRunner.kt
+++ search-api/src/test/kotlin/org/needlehack/searchapi/DummyLoadCommandRunner.kt
@@ -1,15 +1,14 @@
 package org.needlehack.searchapi
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.elasticsearch.ElasticsearchStatusException
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest
-import org.elasticsearch.action.admin.indices.get.GetIndexRequest
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
 import org.elasticsearch.common.xcontent.XContentType
+import org.needlehack.searchapi.controller.SearchController
 import org.needlehack.searchapi.model.FeedItem
 import org.needlehack.searchapi.model.Topic
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
@@ -22,29 +21,30 @@ class DummyLoadCommandRunner(val highLevelclient: RestHighLevelClient,
                              @Value("\${elasticsearch.index}") val indexName: String,
                              val jacksonObjectMapper: ObjectMapper) : CommandLineRunner {
 
+    val log = LoggerFactory.getLogger(SearchController::class.java)
+
     override fun run(vararg args: String?) {
-        for (feedNumber in 1..15 step 2){
-            val item = FeedItem(UUID.randomUUID().toString(),"Dummy article about kotlin", "https://david-romero.github.io//articles/2019-05/jbcnconf-2019", "David Romero",
+        for (feedNumber in 1..15) {
+            val item = FeedItem(UUID.randomUUID().toString(), "Dummy article about kotlin $feedNumber", "https://david-romero.github.io//articles/2019-05/jbcnconf-2019", "David Romero",
                     "Kotlin is awesome", setOf(Topic("kotlin")), Date(), Date())
-            val item2 = FeedItem(UUID.randomUUID().toString(),"Dummy article about kotlin", "https://david-romero.github.io//articles/2019-05/jbcnconf-2019", "David Romero",
-                    "Kotlin is awesome", setOf(Topic("kotlin")), Date(), Date())
-            create(item, item2)
+            create(item)
         }
 
     }
 
-    fun create(firstItem: FeedItem, second: FeedItem) {
-        val json1 = jacksonObjectMapper.writeValueAsString(firstItem)
-        val json2 = jacksonObjectMapper.writeValueAsString(second)
-        val indexRequest = IndexRequest("feed-collector").type("feedItem")
-                .source(XContentType.JSON, json1, json2)
+    fun create(feedItem: FeedItem) {
+        val postMapper: String = jacksonObjectMapper.writeValueAsString(feedItem)
+        val indexRequest = IndexRequest("feed-collector")
+                .id(feedItem.generatedId)
+                .source(postMapper, XContentType.JSON)
 
-        highLevelclient.index(indexRequest, RequestOptions.DEFAULT)
-
+        try {
+            val indexResponse = highLevelclient.index(indexRequest, RequestOptions.DEFAULT)
+            log.debug("New feedItem indexed [{}]", indexResponse)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
     }
 
-    private fun existsIndex(e : ElasticsearchStatusException?): Boolean {
-        val contains = e?.message?.contains("resource_already_exists_exception")
-        return contains?:false
-    }
+
 }
--- search-api/src/test/kotlin/org/needlehack/searchapi/SearchApiApplicationTests.kt
+++ search-api/src/test/kotlin/org/needlehack/searchapi/SearchApiApplicationTests.kt
@@ -1,22 +1,19 @@
 package org.needlehack.searchapi
 
+import org.hamcrest.Matchers.hasSize
 import org.junit.ClassRule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.util.TestPropertyValues
-import org.springframework.context.ApplicationContextInitializer
-import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.testcontainers.elasticsearch.ElasticsearchContainer
 
 
@@ -32,12 +29,11 @@ class SearchApiApplicationTests {
 
     @Test
     fun `given a searching term when the searching is executed then a json with results is returned`() {
-        //TODO: Add TestContainers https://www.testcontainers.org/modules/elasticsearch/
         // given
         val term = "kotlin"
 
         // when
-        var response = mockMvc.perform(MockMvcRequestBuilders
+        val response = mockMvc.perform(MockMvcRequestBuilders
                 .get("/search?term=$term")
                 .accept(APPLICATION_JSON)
                 .contentType(APPLICATION_JSON))
@@ -47,13 +43,54 @@ class SearchApiApplicationTests {
         response
                 .andExpect(status().isOk)
                 .andExpect(content().contentType(APPLICATION_JSON_UTF8))
-                .andExpect(content().string(expectedJson))
+                .andExpect(jsonPath("$.content").isArray)
+                .andExpect(jsonPath("$.content", hasSize<Any>(10)))
+                .andExpect(jsonPath("$.content[0].title").value("Dummy article about kotlin 15"))
+                .andExpect(jsonPath("$.content[0].uri").value("https://david-romero.github.io//articles/2019-05/jbcnconf-2019"))
+                .andExpect(jsonPath("$.content[0].creator").value("David Romero"))
+                .andExpect(jsonPath("$.content[0].content").value("Kotlin is awesome..."))
+                .andExpect(jsonPath("$.content[0].topics").isArray)
+                .andExpect(jsonPath("$.content[0].topics", hasSize<Any>(1)))
+                .andExpect(jsonPath("$.content[0].topics[0]").value("kotlin"))
+                .andExpect(jsonPath("$.numberOfElements").value(10))
+                .andExpect(jsonPath("$.number").value(0))
+                .andExpect(jsonPath("$.size").value(10))
     }
 
-    val expectedJson = "[{\"generatedId\":\"C5FEA1359B85A72B180D4BEEB8D0AE4A\",\"title\":\"Kotlin 1.3.30 released\",\"uri\":\"https://blog.jetbrains.com/kotlin/2019/04/kotlin-1-3-30-released/\",\"creator\":\"Svetlana Isakova\",\"content\":\"<p>We’re happy to announce the release of Kotlin 1.3.30, a new bug fix and tooling update for Kotlin...\",\"topics\":[\"newsletter\",\"Releases\"]},{\"generatedId\":\"7855D1FCE58D58DC2811683D579ED869\",\"title\":\"Kotlin at Trello\",\"uri\":\"http://tech.trello.com/kotlin-at-trello/\",\"creator\":\"\",\"content\":\"...\",\"topics\":[]},{\"generatedId\":\"3239739DFA20D145E6F9F927E06D2CCA\",\"title\":\"Kotlin first impressions\",\"uri\":\"https://blog.pchudzik.com/201711/kotlin-first-impressions/\",\"creator\":\"\",\"content\":\"...\",\"topics\":[]},{\"generatedId\":\"60C9BC57987E5A0F4833456E9372B183\",\"title\":\"Kotlin Census 2018\",\"uri\":\"https://blog.jetbrains.com/kotlin/2018/12/kotlin-census-2018/\",\"creator\":\"Alina Dolgikh\",\"content\":\"<p>Our Kotlin community is growing fast; the number of users has increased by almost 3 times this ye...\",\"topics\":[\"sendtoall\",\"census\",\"community\"]},{\"generatedId\":\"6FA70B4C794DCC7309155D35C7CEC152\",\"title\":\"Kotlin 1.3.40 released\",\"uri\":\"https://blog.jetbrains.com/kotlin/2019/06/kotlin-1-3-40-released/\",\"creator\":\"Eugene Petrenko\",\"content\":\"<p>We’re happy to present the new release today, Kotlin 1.3.40. In addition to the quality and tooli...\",\"topics\":[\"newsletter\",\"Releases\"]},{\"generatedId\":\"8494EFBBD94D8656751260A29D371475\",\"title\":\"Kotlin 1.3.20 released\",\"uri\":\"https://blog.jetbrains.com/kotlin/2019/01/kotlin-1-3-20-released/\",\"creator\":\"Hadi Hariri\",\"content\":\"<p>We’re happy to announce the release of Kotlin 1.3.20, a new bug fix and tooling update for Kotlin...\",\"topics\":[\"newsletter\",\"Releases\",\"sendtoall\"]},{\"generatedId\":\"122CA6AB56EC4B08B223DEECA978E286\",\"title\":\"Gradle Kotlin DSL 1.0\",\"uri\":\"https://blog.jetbrains.com/kotlin/2018/12/gradle-kotlin-dsl-1-0/\",\"creator\":\"Roman Belov\",\"content\":\"<p><center></p>\\n<div style=\\\"background-color: #f1f6fe; margin-bottom: 40px; padding: 15px; line-heig...\",\"topics\":[\"newsletter\",\"guestpost\",\"Tools\",\"community\"]},{\"generatedId\":\"39E45122F1514DEA9B1DB5D462D869E8\",\"title\":\"Lovin’ Kotlin since 2015\",\"uri\":\"http://engineering.vinted.com//2017/05/29/kotlin/\",\"creator\":\"Martynas Jurkus\",\"content\":\"<p>At Google I/O ‘17 the Android team <a href=\\\"https://youtu.be/Y2VF8tmLFHw?t=1h27m\\\">announced first...\",\"topics\":[]},{\"generatedId\":\"2A279766181A0B14D64A53BBFB8986E7\",\"title\":\"Kotlin 1.2.70 is Out!\",\"uri\":\"https://blog.jetbrains.com/kotlin/2018/09/kotlin-1-2-70-is-out/\",\"creator\":\"Sergey Igushkin\",\"content\":\"<p>We’re happy to announce the release of Kotlin 1.2.70, a new bugfix and tooling update for Kotlin ...\",\"topics\":[\"newsletter\",\"Releases\"]},{\"generatedId\":\"FF8D3AD17B7C7A5E5ED2207D6991026E\",\"title\":\"Kotlin 1.3 Released with Coroutines, Kotlin/Native Beta, and more\",\"uri\":\"https://blog.jetbrains.com/kotlin/2018/10/kotlin-1-3/\",\"creator\":\"Roman Belov\",\"content\":\"<p><center></p>\\n<div style=\\\"margin:-30px 0px 10px 0px;padding:10px;border-color:#f0f0f0;display:inli...\",\"topics\":[\"Native\",\"Releases\",\"sendtoall\",\"Events\",\"Tools\",\"Android\"]}]"
+    @Test
+    fun `given a searching term and the second page when the searching is executed then a partial page should be retrieved`() {
+        // given
+        val term = "kotlin"
+        val page = 1
+
+        // when
+        val response = mockMvc.perform(MockMvcRequestBuilders
+                .get("/search?term=$term&page=$page")
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON))
+
+
+        // then
+        response
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.content").isArray)
+                .andExpect(jsonPath("$.content", hasSize<Any>(5)))
+                .andExpect(jsonPath("$.content[0].title").value("Dummy article about kotlin 5"))
+                .andExpect(jsonPath("$.content[0].uri").value("https://david-romero.github.io//articles/2019-05/jbcnconf-2019"))
+                .andExpect(jsonPath("$.content[0].creator").value("David Romero"))
+                .andExpect(jsonPath("$.content[0].content").value("Kotlin is awesome..."))
+                .andExpect(jsonPath("$.content[0].topics").isArray)
+                .andExpect(jsonPath("$.content[0].topics", hasSize<Any>(1)))
+                .andExpect(jsonPath("$.content[0].topics[0]").value("kotlin"))
+                .andExpect(jsonPath("$.numberOfElements").value(5))
+                .andExpect(jsonPath("$.number").value(1))
+                .andExpect(jsonPath("$.size").value(10))
+    }
 
     companion object {
-        @ClassRule @JvmField
+        @ClassRule
+        @JvmField
         val elasticsearchContainer = ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.2.0")
     }
 
